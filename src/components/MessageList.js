c61f6be8745f64904020e5109249ebda3ee5f655
sorahiro0830 <kattyamu0830@gmail.com>
2021-06-22 14:34:38 +0900

    display messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,44 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import { List } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+import MssageItem from './MessageItem';
 import { messagesRef } from '../firebase';
 
 const useStyles = makeStyles({
   root: {
     gridRow: 1,
+    overflow: 'auto',
+    width: '100%',
   },
 });
-
-// 受け取ったときの構造
-// key: -Mc7yxrxAQhjoF2Gh64M, value: {name: "testさん", text: "こんにちは"}
-// 使いやすく整形した構造
-// {key: -Mc7yxrxAQhjoF2Gh64M, name: "testさん", text: "こんにちは"}
 
 const MessageList = () => {
   const [messages, setMessages] = useState([]);
   const classes = useStyles();
 
   useEffect(() => {
-    // console.log('useEffect');
-    messagesRef.orderByKey().limitToLast(3).on('value', (snapshot) => {
-      // console.log(snapshot.val());
+    messagesRef.orderByKey().limitToLast(15).on('value', (snapshot) => {
       const messages = snapshot.val();
-      // console.log(Object.entries(messages));
-      // console.log(messages);
       if (messages === null) return;
       const entries = Object.entries(messages);
       const newMessages = entries.map((entry) => {
-        // console.log(entry);
-        // const key = entry[0];
-        // const nameAndText = entry[1];
         const [key, nameAndText] = entry;
         return { key, ...nameAndText };
       });
-      // console.log(newMessages);
       setMessages(newMessages);
     });
   }, []);
-  return <div className={classes.root}>MessageList</div>;
-}
+  return (
+    <List className={classes.root}>
+      {messages.map(({ key, name, text }) => {
+        return <MssageItem key={key} name={name} text={text}>item</MssageItem>;
+      })}
+    </List>
+  );
+};
 
 export default MessageList;
